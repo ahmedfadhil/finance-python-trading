cdcaf194a29caa8b62fc0ac619166c9614e0c559
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2018-11-03 12:29:43 +0100

    working with OLS

--- multistock.py
+++ multistock.py
@@ -1,4 +1,55 @@
 from main import *
+import statsmodels.api as sm
+
+
 def get(tickers, startdate, enddate):
     def data(ticker):
-        return (pdr.get_data_yahoo())
+        return (pdr.get_data_yahoo(ticker, start=startdate, end=enddate))
+
+    datas = map(data, tickers)
+    return (pd.concat(datas, keys=tickers, names=['Ticker', 'Date']))
+
+
+tickers = ['AAPL', 'MSFT', 'IBM', 'GOOG']
+all_data = get(tickers, datetime.datetime(2006, 10, 1), datetime.datetime(2012, 1, 1))
+
+# Isolate the `Adj Close` values and transform the DataFrame
+daily_close_px = all_data[['Adj Close']].reset_index().pivot('Date', 'Ticker', 'Adj Close')
+
+# Calculate the daily percentage change for `daily_close_px`
+daily_pct_change = daily_close_px.pct_change()
+
+# Plot the distributions
+daily_pct_change.hist(bins=50, sharex=True, figsize=(12, 8))
+
+# Plot a scatter matrix with the `daily_pct_change` data
+pd.scatter_matrix(daily_pct_change, diagonal='kde', alpha=0.1, figsize=(12, 12))
+
+# Isolate the adjusted closing prices
+adj_close_px = aapl['Adj Close']
+
+# Calculate the moving average
+moving_avg = adj_close_px.rolling(window=40).mean()
+
+# Inspect the result
+print(moving_avg[-10:])
+
+# Short moving window rolling mean
+aapl['42'] = adj_close_px.rolling(window=40).mean()
+
+# Long moving window rolling mean
+aapl['252'] = adj_close_px.rolling(window=252).mean()
+
+# Plot the adjusted closing price, the short and long windows of rolling means
+aapl[['Adj Close', '42', '252']].plot()
+
+# Define the minumum of periods to consider
+min_periods = 75
+
+# Calculate the volatility
+vol = daily_pct_change.rolling(min_periods).std() * np.sqrt(min_periods)
+
+# Plot the volatility
+vol.plot(figsize=(10, 8))
+
+# Show the plot
